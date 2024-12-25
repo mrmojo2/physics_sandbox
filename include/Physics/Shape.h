5fc0a402008f05eac9ec1dc969db293045f07673
mrmojo2 <maobadi360@gmail.com>
2024-12-25 19:14:06 +0545

    implemented Polygon and Box shape

--- include/Physics/Shape.h
+++ include/Physics/Shape.h
@@ -1,6 +1,10 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#include "Vector.h"
+
+#include <vector>
+
 enum ShapeType {
 	CIRCLE,
 	POLYGON,
@@ -28,7 +32,26 @@ public:
 };
 
 class Polygon:public Shape{
-//
+public:
+	std::vector<Vec2> vertices;
+
+	Polygon(std::vector<Vec2> vertices);
+	Polygon()=default;
+	~Polygon();
+	ShapeType getShapeType() const;
+	Shape* getPointer() const;
+	float getMOI(float mass) const;
+};
+
+class Box:public Polygon {
+public:
+	float width,height;
+	
+	Box(float, float);
+	~Box();
+	ShapeType getShapeType() const;
+	Shape* getPointer() const;
+	float getMOI(float mass) const;
 };
 
 
--- src/Physics/Shape.cpp
+++ src/Physics/Shape.cpp
@@ -1,5 +1,11 @@
 #include "Shape.h"
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				Circle Shape Implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 Circle::Circle(float radius):radius(radius){
 }
 
@@ -19,3 +25,43 @@ float Circle::getMOI(float mass) const {
 	return (0.5 * mass * radius * radius);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				Polygon Shape Implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Polygon::Polygon(std::vector<Vec2> vertices):vertices(vertices){}
+
+Polygon::~Polygon(){}
+
+ShapeType Polygon::getShapeType() const {
+	return POLYGON;
+}
+
+float Polygon::getMOI(float mass) const {
+	return 0.0;  //will implement later
+}
+
+Shape* Polygon::getPointer() const {
+	return new Polygon(this->vertices);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//				BOX Shape Implementation
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Box::Box(float width, float height): width(width),height(height){
+}
+
+Box::~Box(){}
+
+ShapeType Box::getShapeType() const {
+	return BOX;
+}
+
+float Box::getMOI(float mass) const {
+	return 0.08333333333 * mass * (width*width + height*height);
+}
+
+Shape* Box::getPointer() const {
+	return new Box(width, height);
+}
